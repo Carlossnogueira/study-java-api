e8b80f38769c9efdf25eb1ba8f78f5a39489e525
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-06-30 20:58:01 -0300

    tratando erros / adicionando banco de dados com jpa

--- src/main/java/br/com/webapi/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/webapi/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.webapi.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +14,14 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @NotBlank(message= "O nome não pode ficar vazio!")
+    @Pattern(regexp = "\\S+", message = "A campo username não pode conter espaços!")
     private String userame;
+    
+    @Email(message = "O campo precisa de um email válido!")
     private String email;
+    @Length(min = 5, max = 10)
     private String password;
     private String curriculum;
 
